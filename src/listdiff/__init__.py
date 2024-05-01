baaef9cb8714077275abc0465857cd659942ade4
Rehan Khwaja <rehan@khwaja.name>
2024-04-30 17:06:11 -0700

    Add ruff formatting

--- src/listdiff/__init__.py
+++ src/listdiff/__init__.py
@@ -21,20 +21,22 @@ T = TypeVar('T')
 U = TypeVar('U')
 K = TypeVar('K')
 
+
 class SupportsCompare(Protocol[K]):
-	def __lt__(self, other: Self) -> bool:
-		...
+	def __lt__(self, other: Self) -> bool: ...
 
-	def __gt__(self, other: Self) -> bool:
-		...
+	def __gt__(self, other: Self) -> bool: ...
 
-def DiffUnsortedLists(listA: Iterable[T], listB: Iterable[U], keyA: Callable[[T], SupportsCompare[K]], keyB: Callable[[U], SupportsCompare[K]]) -> tuple[list[T], list[tuple[T,U]], list[U]]:
+
+def DiffUnsortedLists(listA: Iterable[T], listB: Iterable[U], keyA: Callable[[T], SupportsCompare[K]], keyB: Callable[[U], SupportsCompare[K]]) -> tuple[list[T], list[tuple[T, U]], list[U]]:
 	"""iterators point to unsorted lists but the given keys represent their identities for comparison"""
 	return DiffListsByKey(iter(sorted(listA, key=keyA)), iter(sorted(listB, key=keyB)), keyA, keyB)
 
-def DiffListsByKey(iterA: Iterator[T], iterB: Iterator[U], keyA: Callable[[T], SupportsCompare[K]], keyB: Callable[[U], SupportsCompare[K]]) -> tuple[list[T], list[tuple[T,U]], list[U]]:
+
+def DiffListsByKey(iterA: Iterator[T], iterB: Iterator[U], keyA: Callable[[T], SupportsCompare[K]], keyB: Callable[[U], SupportsCompare[K]]) -> tuple[list[T], list[tuple[T, U]], list[U]]:
 	"""iterators point to lists sorted by the given keys, which also represent their identities for comparison"""
 	return _DiffLists(iterA, iterB, lambda a, b: -1 if keyA(a) < keyB(b) else 1 if keyA(a) > keyB(b) else 0)
+
 
 # assumes iterA and iterB are ordered
 def _DiffLists(iterA, iterB, compare):
--- tests/test_all.py
+++ tests/test_all.py
@@ -2,6 +2,7 @@ from pytest import mark
 
 from listdiff import _DiffLists, DiffListsByKey, DiffUnsortedLists
 
+
 def test_diff_unsorted_lists():
 	listA = [3, 2, 1]
 	listB = [5, 4, 3]
@@ -9,6 +10,7 @@ def test_diff_unsorted_lists():
 	assert inA == [1, 2]
 	assert inBoth == [(3, 3)]
 	assert inB == [4, 5]
+
 
 def test_diff_lists_by_key():
 	listA = [1, 2, 3]
@@ -18,12 +20,17 @@ def test_diff_lists_by_key():
 	assert inBoth == [(3, 3)]
 	assert inB == [4, 5]
 
-@mark.parametrize(('listA', 'listB', 'expected'),
-	[ ([1, 2, 3, 5], [2, 3, 4], ([1, 5], [(2, 2), (3, 3)], [4]))
-	, ([], [2, 3, 4], ([], [], [2, 3, 4]))
-	, ([1, 2, 3, 5], [], ([1, 2, 3, 5], [], []))
-	, ([], [], ([], [], []))
-	, ([1, 2, 3], [4, 5, 6], ([1, 2, 3], [], [4, 5, 6])) ])
+
+@mark.parametrize(
+	('listA', 'listB', 'expected'),
+	[
+		([1, 2, 3, 5], [2, 3, 4], ([1, 5], [(2, 2), (3, 3)], [4])),
+		([], [2, 3, 4], ([], [], [2, 3, 4])),
+		([1, 2, 3, 5], [], ([1, 2, 3, 5], [], [])),
+		([], [], ([], [], [])),
+		([1, 2, 3], [4, 5, 6], ([1, 2, 3], [], [4, 5, 6])),
+	],
+)
 def test_parameterized(listA, listB, expected):
 	resultA = _DiffLists(iterA=iter(listA), iterB=iter(listB), compare=lambda x, y: (x > y) - (y > x))
 	resultB = DiffListsByKey(iterA=iter(listA), iterB=iter(listB), keyA=lambda x: x, keyB=lambda x: x)
