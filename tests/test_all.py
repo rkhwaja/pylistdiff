4c1b363ed09aa6195ace9589a16da8fba9cf8131
Rehan Khwaja <rehan@khwaja.name>
2024-05-23 23:23:59 -0700

    Format with yapf

--- tests/test_all.py
+++ tests/test_all.py
@@ -18,12 +18,8 @@ def test_diff_lists_by_key():
 	assert inBoth == [(3, 3)]
 	assert inB == [4, 5]
 
-@mark.parametrize('listA,listB,expected',
-	[ ([1, 2, 3, 5], [2, 3, 4], ([1, 5], [(2, 2), (3, 3)], [4]))
-	, ([], [2, 3, 4], ([], [], [2, 3, 4]))
-	, ([1, 2, 3, 5], [], ([1, 2, 3, 5], [], []))
-	, ([], [], ([], [], []))
-	, ([1, 2, 3], [4, 5, 6], ([1, 2, 3], [], [4, 5, 6])) ])
+@mark.parametrize('listA,listB,expected', [([1, 2, 3, 5], [2, 3, 4], ([1, 5], [(2, 2), (3, 3)], [4])), ([], [2, 3, 4], ([], [], [2, 3, 4])), ([1, 2, 3, 5], [], ([1, 2, 3, 5], [], [])),
+	([], [], ([], [], [])), ([1, 2, 3], [4, 5, 6], ([1, 2, 3], [], [4, 5, 6]))])
 def test_parameterized(listA, listB, expected):
 	resultA = _DiffLists(iterA=iter(listA), iterB=iter(listB), compare=lambda x, y: (x > y) - (y > x))
 	resultB = DiffListsByKey(iterA=iter(listA), iterB=iter(listB), keyA=lambda x: x, keyB=lambda x: x)
